3a511e26e8ecfa28913186482b5da4c4307dd70e
m-evtimov96 <m.evtimov196@gmail.com>
2024-08-02 23:43:36 +0300

    Order by date desc in JobAdViewSet

--- job_assistant/crawlers/api/views.py
+++ job_assistant/crawlers/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from job_assistant.crawlers.api.serializers import JobAdSerializer
 from job_assistant.crawlers.models import JobAd
 from django_filters.rest_framework import DjangoFilterBackend
@@ -8,6 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class JobAdViewSet(ReadOnlyModelViewSet):
     queryset = JobAd.objects.all()
     serializer_class = JobAdSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["workplace"]
     search_fields = ["categories"]
+    ordering = ["-date"]
