ff4ac7a49814a3f5da3ce6f07e8d870e52255ded
m-evtimov96 <m.evtimov196@gmail.com>
2024-08-23 11:47:41 +0300

    Add SoftDelete queryset with working delete method

--- job_assistant/crawlers/managers.py
+++ job_assistant/crawlers/managers.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class SoftDeleteManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_deleted=False)
+        return SoftDeleteQuerySet(model=self.model, using=self._db).filter(is_deleted=False)
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
